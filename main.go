1fecb2459485d0d5d83af694d783bd07b9c694bc
Adam Burgess <adamburgess7890@gmail.com>
2022-11-13 07:21:44 +0000

    Refactor of user state into it's own package

--- main.go
+++ main.go
@@ -1,12 +1,10 @@
 package main
 
 import (
-	"encoding/json"
 	"fmt"
-	"os"
 	"packages/exercise"
 	"packages/inputters"
-	"strings"
+	"packages/startup"
 )
 
 func initialiseExcerise(ue *exercise.UsersExercise,name string){
@@ -25,65 +23,6 @@ func addIteration(ue *exercise.UsersExercise, name string, x exercise.Iteration)
 		return;
 	}
 	ue.Exercises[name] = entry;
-}
-
-func saveUser(ue *exercise.UsersExercise){
-	file, _ := json.MarshalIndent(ue, "", " ")
-	err := os.WriteFile(username+".json", file, 0644)
-    if err == nil{
-		fmt.Printf("Succesful save for user %s\n", username);
-	}
-}
-
-func newUser() *exercise.UsersExercise{
-	username = inputters.FetchString("Please enter what you would like your username to be");
-	var ue = &exercise.UsersExercise{Exercises: map[string]exercise.Exercise{}};
-	return ue;
-}
-
-func loadUser() * exercise.UsersExercise{
-	
-	var ue = &exercise.UsersExercise{Exercises: map[string]exercise.Exercise{}};
-	var data []byte;
-	for{
-		username = inputters.FetchString("Please enter your username:");
-		fmt.Println(username+".json")
-		dat, err := os.ReadFile(username+".json")
-		if err != nil{
-			fmt.Printf("Not a valid username, please try again\n")
-			continue;
-		}
-		data = dat;
-		break;
-	}
-	
-	json.Unmarshal([]byte(data), &ue)
-	backupfile()
-	return ue;
-}
-
-func newLine(){
-	fmt.Printf("================================================================================\n")
-}
-
-func backupfile(){
-	dat, _ := os.ReadFile(username+".json")
-	dateFileFormat := strings.Replace(date, "/", ":", 2);
-	err := os.WriteFile(username+"-BACKUP:" + dateFileFormat+ ".json", dat, 0644)
-	if err != nil{
-		fmt.Printf("Unsuccesful backup of: %s" ,username)
-	}
-}
-
-
-func startUp() * exercise.UsersExercise {
-	fmt.Printf("Welcome!\n")
-	integerInput := inputters.FetchInteger("[1] Log in [2] Create Account\n",2)
-	fmt.Println(integerInput)
-	if(integerInput ==1){
-		return loadUser();
-	}
-	return newUser();
 }
 
 func newIteration(reps []float32, weights []float32, variances []float32, ID int, sets []int, weight float32, date string, note string, totalWeight float32, averageRep float32, averageWeightRepTotal float32) *exercise.Iteration{
@@ -114,6 +53,32 @@ func main() {
     test := inputters.FetchInteger("hello wolrd!",2)
     fmt.Println(test)
 
-    userInfo = startUp()
+    userInfo = startup.StartUp()
     fmt.Println(userInfo)
+	startup.SaveUser(userInfo)
 }
+
+
+/*Functions I need to make:
+	Log in
+	Create new Account
+        //Move this to a different package! 
+	Add new excerise instance
+	Views:
+		Most recent
+		Average overall
+		Standard
+		Simple
+	Delete:
+		One instance
+		All instances
+	View all exercise names
+	Save
+
+	Type in an array of doubles only
+	Type in a number only
+	Type in a string only
+
+	Mapping to the map....therefore numbers for each of the excerises. 
+		Do this with a function and on this new version of my JSON we can actually log the IDs too....? 
+*/
